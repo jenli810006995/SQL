1a105a048e3bb6215b8b2742e9ef259b1f1b24be
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-11-21 11:41:04 -0500

    Create BytesStringInts.sql

---
+++ CodewarsSQL/7kyu/SQL: Bytes in String from Ints/BytesStringInts.sql
@@ -0,0 +1,7 @@
+select length(number1::text) as octnum1,
+       length(number2::text) as octnum2,
+       length(number3::text) as octnum3,
+       length(number4::text) as octnum4,
+       length(number5::text) as octnum5
+from numbers;
+
