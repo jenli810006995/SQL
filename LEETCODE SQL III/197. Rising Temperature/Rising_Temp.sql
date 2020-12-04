3e6febabb56259b7fa1b251ddb73bbe580fdcd44
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-12-03 20:35:31 -0500

    Update Rising_Temp.sql

--- LEETCODE SQL III/197. Rising Temperature/Rising_Temp.sql
+++ LEETCODE SQL III/197. Rising Temperature/Rising_Temp.sql
@@ -5,3 +5,11 @@ from Weather
 join Weather w on datediff(Weather.recordDate, w.recordDate) = 1
 and Weather.Temperature > w.Temperature;
 
+
+--- one of the fastest solution
+
+select w1.id as id
+from Weather w1, Weather w2
+where w1.Temperature > w2.Temperature
+and subdate(w1.recordDate, 1) = w2.recordDate;
+
