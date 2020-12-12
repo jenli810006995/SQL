8290b65ce9976f5e7f9d756762bf45e3cc966316
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-12-12 11:18:22 -0500

    Update Reported_posts.sql

--- LEETCODE SQL III/1132. Reported Posts II/Reported_posts.sql
+++ LEETCODE SQL III/1132. Reported Posts II/Reported_posts.sql
@@ -15,3 +15,16 @@ group by 1
 ) t1
 
 -- Link: https://leetcode.com/problems/reported-posts-ii/
+
+
+select round(100*avg(rate), 2) as average_daily_percent
+from
+(
+    select a.action_date,
+           sum(a.action_date <= r.remove_date and a.post_id = r.post_id)/count(distinct a.post_id) as rate
+    from ( select distinct post_id, action_date, extra
+          from Actions) a, Removals r
+    where
+    a.extra = 'spam'
+    group by 1
+) t
