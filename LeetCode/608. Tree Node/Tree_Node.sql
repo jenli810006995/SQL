66a1ea9807221f2256408e80c9ccdf8097939090
Jen <38231830+jenli810006995@users.noreply.github.com>
2021-01-18 11:42:00 -0500

    Create Tree_Node.sql

---
+++ LeetCode/608. Tree Node/Tree_Node.sql
@@ -0,0 +1,8 @@
+select id,
+       case when tree.id = (select atree.id from tree atree where p_id is null) then 'Root'
+            when tree.id in (select p_id from tree atree where p_id is not null) then 'Inner'
+            else 'Leaf' end as type
+from tree 
+order by id;
+
+-- Link: https://leetcode.com/problems/tree-node/
